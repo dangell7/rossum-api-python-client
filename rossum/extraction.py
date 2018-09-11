3c1b55d57a63482d53af6d8514ae25a9df6feaef
Martin Holeček <martin.holecek@rossum.ai>
2018-09-11 12:47:59 +0200

    add locale option to extract document dates in specified locale

--- rossum/extraction.py
+++ rossum/extraction.py
@@ -51,11 +51,12 @@ class ElisExtractionApi(object):
         if base_url.endswith("/"):
             base_url = base_url[:-1]
         self.base_url = base_url
+        self.default_locale = 'en_GB'
 
         # we do not use requests.auth.HTTPBasicAuth
         self.headers = {'Authorization': 'secret_key ' + self.api_key}
 
-    def extract(self, document_file, output_file=None, filter='best'):
+    def extract(self, document_file, output_file=None, filter='best', locale=None):
         """
         Extracts a document using Elis Extraction API.
 
@@ -71,9 +72,15 @@ class ElisExtractionApi(object):
             applied may change over time.
             all - Returns the complete set of extracted fields, even lower
             quality ones. The client has to post-process the fields appropriately.
+        :param locale: (optional)
+            A hint for Elis that may help her to extract certain fields, which may depend on the locale, correctly.
+            For example, in US the typical date format is mm.dd.yyyy whilst in Czech it is dd.mm.yyyy.
+            So date such as 12. 6. 2018 when locale=cz_CZ is specified is going to be extracted as 12th of June,
+            while when locale=en_US is used the date is going to be extracted as 6th of December 2018.
+            When passed None, uses self.default_locale.
         :return: dict with extractions, see the documentation for details
         """
-        send_result = self.send_document(document_file)
+        send_result = self.send_document(document_file, locale)
         document_id = send_result['id']
         extraction = self.get_document(document_id, filter=filter, verbose=True)
         if extraction['status'] == 'error':
@@ -86,16 +93,18 @@ class ElisExtractionApi(object):
 
         return extraction
 
-    def send_document(self, document_path):
+    def send_document(self, document_path, locale=None):
         """
         Submits a document to Elis Extraction API for extractions.
 
         Returns: dict with 'id' representing job id
         """
+        if locale is None:
+            locale = self.default_locale
         with open(document_path, 'rb') as f:
             content_type = self._content_type(document_path)
             response = requests.post(
-                '%s/document' % self.base_url,
+                '%s/document?locale=%s' % (self.base_url, locale),
                 files={'file': (os.path.basename(document_path), f, content_type)},
                 headers=self.headers)
         result = json.loads(response.text)
